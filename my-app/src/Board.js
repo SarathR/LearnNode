ffa743306cde5194a3303e92fcd1ac0e57a0afd5
sarathr <sarathr@gmail.com>
2020-07-25 16:42:55 +0530

    Moved the code to loop to render squares

--- my-app/src/Board.js
+++ my-app/src/Board.js
@@ -2,31 +2,29 @@ import Square from './Square';
 import React from 'react';
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.rows = Array(3).fill(null);
+  }
   renderSquare(i) {
-    return (<Square 
+    return (<Square
+      key={i} 
       value={this.props.squares[i]}
       onClick={() => this.props.onClick(i)}
     />);
+  }
+  renderRow(row) {
+    return (
+      <div key={row} className="board-row">
+        {this.rows.map((x, col) => this.renderSquare(3*row+col))}
+      </div>
+    );
   }
 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.rows.map((x, rowIndex) => this.renderRow(rowIndex))}
       </div>
     );
   }
