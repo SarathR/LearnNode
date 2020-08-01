7b0f522c0a085892b796079ac85b55144b98af7f
sarathr <sarathr@gmail.com>
2020-08-01 17:08:24 +0530

    Updates

---
+++ precache-manifest.ab906f0ec2884218445fce5a4cc2de2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361df7efd09b691cf584556e59e59e59",
+    "url": "/try-react-deploy/index.html"
+  },
+  {
+    "revision": "a5a3f477e18f032b8e06",
+    "url": "/try-react-deploy/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "06ee483bf3493c13ac2f",
+    "url": "/try-react-deploy/static/js/2.bf928809.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/try-react-deploy/static/js/2.bf928809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a3f477e18f032b8e06",
+    "url": "/try-react-deploy/static/js/main.66ab53db.chunk.js"
+  },
+  {
+    "revision": "f20f2d81bc5d9632af08",
+    "url": "/try-react-deploy/static/js/runtime-main.d024f04b.js"
+  }
+]);
